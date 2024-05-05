0e6d4aad31803a5c52db045f332fefa62db583c7
Michael Commo <mcommo@outlook.com>
2024-05-05 12:50:42 -0400

    initial commit - upgraded and publishing
    Upgraded existing code-base from .net 4.5 to .net8

---
+++ PdfMergeUtility/PdfMergeService.cs
@@ -0,0 +1,65 @@
+﻿using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfMergeUtility
+{
+    internal class PdfMergeService
+    {
+        public void Merge(FileInfo front, FileInfo back, FileInfo? targetFile)
+        {
+            using (PdfDocument pdfFront = PdfReader.Open(front.FullName, PdfDocumentOpenMode.Import))
+            using (PdfDocument pdfBack = PdfReader.Open(back.FullName, PdfDocumentOpenMode.Import))
+            using (PdfDocument outPdf = new PdfDocument())
+            {
+                var pageCounts = pdfFront.PageCount + pdfBack.PageCount;
+
+                if (pdfFront.PageCount != pdfBack.PageCount)
+                    throw new InvalidOperationException($"Failed to merge pdfs from {front.Name} and {back.Name}. Page counts must match.");
+
+                for (int i = 0; i < pageCounts; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        // append front (take from front)
+                        AppendPage(pdfFront, i / 2, outPdf);
+                    }
+                    else
+                    {
+                        // append back (take from back)
+                        AppendPage(pdfBack, i / 2, outPdf);
+
+                    }
+                }
+
+                if (targetFile is null)
+                {
+                    outPdf.Save(Path.Combine(front.Directory.FullName, front.Name.Replace(front.Extension, "-new.pdf")));
+                } else
+                {
+                    outPdf.Save(Path.Combine(targetFile.Directory.FullName, targetFile.Name.Replace(targetFile.Extension, ".pdf")));
+                }
+            }
+
+            //front.Delete();
+            //back.Delete();
+        }
+
+        private void CopyPages(PdfDocument from, PdfDocument to)
+        {
+            for (int i = 0; i < from.PageCount; i++)
+            {
+                to.AddPage(from.Pages[i]);
+            }
+        }
+
+        private void AppendPage(PdfDocument source, int pageNumber, PdfDocument target)
+        {
+            target.AddPage(source.Pages[pageNumber]);
+        }
+    }
+}
---
+++ PdfMergeUtility/Program.cs
@@ -0,0 +1,98 @@
+﻿using PdfMergeUtility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfMergeUtility
+{
+    class Program
+    {
+        private const string frontFileExtension = ".pdff";
+        private const string backFileExtension = ".pdfb";
+        private static string[] FileExtensions = new[]
+        {
+                frontFileExtension, backFileExtension
+        };
+
+        public Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(Utilities.AssemblyResolve);
+        }
+
+        static void Main(string[] args)
+        {
+            FileInfo file1Info;
+            FileInfo file2Info;
+
+            var mergeService = new PdfMergeService();
+            //var tuplesOfPdfsToMerge = new List<Tuple<FileInfo, FileInfo>>();
+
+            // get file 1 from user
+            Console.WriteLine("Enter path to the file containing pages 1, 3, 5, etc...");
+            var file1 = Console.ReadLine();
+            try
+            {
+                file1Info = new FileInfo(file1);
+                if (!file1Info.Exists) throw new Exception();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Could not find file {file1}. Try Again...");
+            }
+
+            // get file 2 from user
+            Console.WriteLine("Enter path to the file containing pages 2, 4, 6, etc...");
+            var file2 = Console.ReadLine();
+            try
+            {
+                file2Info = new FileInfo(file2);
+                if (!file2Info.Exists) throw new Exception();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Could not find file {file2}. Try Again...");
+            }
+
+            // get desitination file from user
+            Console.WriteLine("Path and name to store the resulting file?");
+            var targetFile = Console.ReadLine();
+            var targetFileInfo = new FileInfo(targetFile);
+
+            //var currentDirectory = Environment.CurrentDirectory;
+            //Console.WriteLine($"Working Directory: {currentDirectory}");
+            //var files = Utilities.GetFilesInDirectoryRecursive(currentDirectory, FileExtensions);
+
+            //// group the pdfs into sets of fronts and backs
+            //foreach (var frontFile in files.Where(x => x.Extension.Equals(frontFileExtension)))
+            //{
+            //    var backFile = files.Where(x => x.Extension.Equals(backFileExtension));
+
+            //    if (backFile.Count() == 1)
+            //    {
+            //        tuplesOfPdfsToMerge.Add(new Tuple<FileInfo, FileInfo>(frontFile, backFile.Single()));
+            //    }
+            //}
+
+            // merge the two pdfs
+            try
+            {
+                mergeService.Merge(file1Info, file2Info, targetFileInfo);
+                Console.WriteLine("Completed Successfully.");
+                Console.WriteLine("Press any key to exit...");
+
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex);
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
+            }         
+
+        }
+    }
+}
